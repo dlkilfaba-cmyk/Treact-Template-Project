c44610fe6a8a13e79e94bacbd2723f5a720d899c
dlkilfaba-cmyk <fed.dk69@gmail.com>
2026-03-21 19:52:27 -0400

    Fix HTML errors and improve code quality

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ if (getStartedBtn) {
 		console.log("Get Started button clicked.");
 	});
 }
+
+let elements = document.getElementsByClassName('nav__link--burger')
+
+
+function menuOpen(){
+    document.body.classList += 'open--menu'
+
+    for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add('nav__link--burger__custom')
+}
+
+}
+
+function menuClose(){
+    document.body.classList.remove('open--menu')
+
+    for(let i = 0; i < elements.length; i++){
+        elements[i].classList.remove('nav__link--burger__custom')
+    }
+
+}
